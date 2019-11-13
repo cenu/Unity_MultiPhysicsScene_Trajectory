25c97b863bd42f5f6a3123ce8175094f53131eae
DESKTOP-BAHAMUT\bahamut <armoredcode@gmail.com>
2019-11-13 15:48:37 +0900

    Add : Trajectory with Multi-Physics Scene

---
+++ Assets/BallActor.cs
@@ -0,0 +1,68 @@
+﻿
+using UnityEngine;
+
+public class BallActor : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    private bool _play;
+    private float _potential;
+
+    public Vector3 position { get { return _rigidBody.position; } }
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _rigidBody.isKinematic = true;
+    }
+
+    public void SetRenderable(bool renderable)
+    {
+        var renderers = GetComponentsInChildren<Renderer>();
+        foreach (var ren in renderers)
+            ren.enabled = renderable;
+    }
+
+    public void Warp(Vector3 pos)
+    {
+        _rigidBody.position = pos;
+        _rigidBody.velocity = Vector3.zero;
+        _rigidBody.angularVelocity = Vector3.zero;
+    }
+
+    public void Play(float potential)
+    {
+        _rigidBody.isKinematic = false;
+        _play = true;
+        _potential = potential;
+
+        _dbg_Log = 0;
+    }
+
+    public void Stop()
+    {
+        _rigidBody.isKinematic = true;
+        _play = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    int _dbg_Log;
+    public void Simulate(float fixedDeltaTime)
+    {
+        if (_play && 0 < _potential)
+        {
+            _rigidBody.AddForce(new Vector3(0, 0, _potential), ForceMode.Force);
+            _potential -= fixedDeltaTime;
+        }
+    }
+}
---
+++ Assets/PhysicsTest2.cs
@@ -0,0 +1,142 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhysicsTest2 : MonoBehaviour
+{
+    [SerializeField] private GameObject ball;
+    [SerializeField] private GameObject field;
+    [SerializeField] private LineRenderer line;
+    [SerializeField] private float simulateFrame;
+    [SerializeField] private float simulateSpeed;
+    [SerializeField] private float ballPotential;
+
+    private Scene _mainScene;
+    private PhysicsScene _mainPhysicsScene;
+
+    private Scene _subScene;
+    private PhysicsScene _subPhysicsScene;
+
+    private Vector3 _ballPosition;
+
+    private BallActor _ballActor;
+
+    private GameObject _hiddenBall;
+    private BallActor _hiddenBallActor;
+
+    private float _timer;
+
+    private void Awake()
+    {
+        _mainScene = SceneManager.GetActiveScene();
+        _mainPhysicsScene = _mainScene.GetPhysicsScene();
+
+        _subScene = SceneManager.CreateScene("PhysicsScene1", new CreateSceneParameters(LocalPhysicsMode.Physics3D));
+        _subPhysicsScene = _subScene.GetPhysicsScene();
+
+        _ballActor = ball.GetComponent<BallActor>();
+        _ballPosition = new Vector3(0, 2, 0);
+
+        Physics.autoSimulation = false;
+    }
+
+    private void Start()
+    {
+        CreateTrajectory();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _ballActor.Play(ballPotential);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _ballPosition = new Vector3(0, 2, 0);
+
+            _ballActor.Stop();
+            _ballActor.Warp(_ballPosition);
+            CreateTrajectory();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _ballPosition += new Vector3(-0.1f, 0, 0);
+            _ballActor.Warp(_ballPosition);
+            CreateTrajectory();
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _ballPosition += new Vector3(0.1f, 0, 0);
+            _ballActor.Warp(_ballPosition);
+            CreateTrajectory();
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _ballPosition += new Vector3(0, 0, 0.1f);
+            _ballActor.Warp(_ballPosition);
+            CreateTrajectory();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _ballPosition += new Vector3(0, 0, -0.1f);
+            _ballActor.Warp(_ballPosition);
+            CreateTrajectory();
+        }
+
+        _timer += Time.deltaTime * 0.5f;
+        while (_timer >= Time.fixedDeltaTime)
+        {
+            _timer -= Time.fixedDeltaTime;
+            _mainPhysicsScene.Simulate(Time.fixedDeltaTime);
+            _ballActor.Simulate(Time.fixedDeltaTime);
+        }
+    }
+
+    private void CreateTrajectory()
+    {
+        if (_hiddenBall == null)
+        {
+            _hiddenBall = Instantiate(ball);
+            _hiddenBall.name = "HiddenBall";
+
+            _hiddenBallActor = _hiddenBall.GetComponent<BallActor>();
+            _hiddenBallActor.SetRenderable(false);
+
+            SceneManager.MoveGameObjectToScene(_hiddenBall, _subScene);
+
+            var hiddenField = Instantiate(field);
+            SceneManager.MoveGameObjectToScene(hiddenField, _subScene);
+
+            var ren2 = hiddenField.GetComponentsInChildren<Renderer>();
+            foreach (var r in ren2)
+                r.enabled = false;
+        }
+
+        if (_hiddenBall != null)
+        {
+            _hiddenBallActor.Warp(_ballActor.position);
+            _hiddenBallActor.Play(ballPotential);
+
+            List<Vector3> points = new List<Vector3>();
+            for (int i = 0; i < simulateFrame; i++)
+            {
+                float dt = Time.fixedDeltaTime; ;
+                _hiddenBallActor.Simulate(dt);
+                _subPhysicsScene.Simulate(dt);
+
+                if (points.Count == 0 || (0.2f < (points[points.Count - 1] - _hiddenBallActor.position).magnitude))
+                    points.Add(_hiddenBallActor.position);
+            }
+
+            line.positionCount = points.Count;
+            line.SetPositions(points.ToArray());
+        }
+    }
+}
